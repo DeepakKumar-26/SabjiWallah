bf7feddd4f80132af88367bcb587c27dd404d02a
Deepak Kumar <deepakkum261626@gmail.com>
2024-05-24 18:34:12 +0530

    configuring ios apps to receive app updates from codepush

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import CodePush from 'react-native-code-push';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigation/StackNavigation';
 
-export default function App() {
+const App=()=> {
   return (
     <NavigationContainer>
       <StackNavigation />
@@ -12,3 +13,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({});
+
+export default CodePush(App);
